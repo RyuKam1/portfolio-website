b6972e722e77e9f206fa86d530385a6376bb9222
RyukASF <wismyname15@gmail.com>
2024-10-03 00:15:48 +0400

    first responsive update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,23 +6,35 @@ function Footer() {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.container}>
-        <div className={styles.footerText}>@all rights reserved</div>
+        <div className={styles.footerText}>All Rights Reserved™</div>
+
         <div className={styles.socialNetworks}>
-          <Image
-            src="/1.png"
-            className={styles.icon}
-            width={20}
-            height={20}
-            alt="facebook"
-          />
-          <Image
-            src="/2.png"
-            className={styles.icon}
-            width={20}
-            height={20}
-            alt="instagram"
-          />
-          <Image
+          <a
+            href="https://www.facebook.com/profile.php?id=61552704748659"
+            target="_blank"
+          >
+            <Image
+              src="/1.png"
+              className={styles.icon}
+              width={20}
+              height={20}
+              alt="facebook"
+            />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/giorgi-omarashvili-511956309/"
+            target="_blank"
+          >
+            <Image
+              src="/2.png"
+              className={styles.icon}
+              width={20}
+              height={20}
+              alt="instagram"
+            />
+          </a>
+          {/* <Image
             src="/3.png"
             className={styles.icon}
             width={20}
@@ -35,7 +47,7 @@ function Footer() {
             width={20}
             height={20}
             alt="youtube"
-          />
+          /> */}
         </div>
       </div>
     </div>
